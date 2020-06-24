04dd929c2f5e82f8cc9bd925082791d8c4799ee9
gitWxk <wxk@email.com>
2020-06-24 18:35:29 +0800

    增加项目

---
+++ WxkNetKit/WxkNetManager.m
@@ -0,0 +1,15 @@
+//
+//  WxkNetManager.m
+//  WxkNetKit
+//
+//  Created by 王晓康 on 2020/6/24.
+//  Copyright © 2020 wangxiaokang. All rights reserved.
+//
+
+#import "WxkNetManager.h"
+
+@implementation WxkNetManager
+- (void)wxkTest {
+    NSLog(@"--------%s",__func__);
+}
+@end
